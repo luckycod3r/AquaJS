752cd10ad21ad63ed8699f1f04844df4fdc6cdd2
luckycod3r <94208991+luckycod3r@users.noreply.github.com>
2022-04-25 18:44:37 +1100

    Delete minimize.py

--- minimize.py
+++
@@ -1,6 +0,0 @@
-import requests
-url = 'https://www.toptal.com/developers/javascript-minifier/raw'
-data = {'input': open('build/AquaJS.js', 'rb').read()}
-response = requests.post(url, data=data)
-minimze = open("build/AquaJS.min.js","w")
-minimze.write(response.text)
