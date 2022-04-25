39218c3e0e08a33b859e2b3e29139b6879fc426a
luckycod3r <94208991+luckycod3r@users.noreply.github.com>
2022-04-25 18:45:02 +1100

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-let wrapper = A_(".wrapper").e;
-let r = A_("sort",[0,3,1,3,6,2,8,9,7]);
-
-A_("response","GET","https://aquaclouds.ru/api/v1/get/luckyhash/?action=hash&text=123api-key=desktopapp",{
-    key : "d"
-})
-console.log(r);
